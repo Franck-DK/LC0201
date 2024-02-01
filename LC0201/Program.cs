77d8f4cdfe10c2f1221ea0430e37b22e48259f8b
Franck-DK <franck.dang.kum-1@ou.edu>
2024-02-01 01:21:38 -0600

    Add project files.

---
+++ LC0201/Program.cs
@@ -0,0 +1,50 @@
+﻿//MIS 3033 003
+//Feb 1, 2024
+//Franck D K
+//113537021
+
+//delegate: a key word to define a functiom type
+//class to define a new datatype
+using f1;
+
+Console.WriteLine("delegate");
+
+Student stu1;
+
+bool CheckAge(Student stu)
+{
+    if(stu.age > 20)
+    {
+        return true;
+    }
+    else 
+    { 
+        return false; 
+    }
+}
+
+bool CheckGrade(Student stu)// Check grade is greater than or equal to 80
+{
+    if (stu.grade >= 80)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+FunctionType1 funType1Card;
+funType1Card = CheckAge;
+
+stu1 = new Student();
+stu1.Name = "tom";
+stu1.age = 20;
+stu1.grade = 90;
+stu1.weight = 128.9;
+
+Console.WriteLine(funType1Card(stu1));
+
+//define a delegate at the end of the code
+delegate bool FunctionType1(Student stu); //we defined a function type
---
+++ LC0201/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace f1
+{
+    public class Student// complex
+        {
+        public string Name;
+        public int age;
+        public double weight;
+        public double grade;
+    }
+}
